3e51cd69ce2d5a4dbeb64e1b5b94c64877f7c799
aphiwe <blomaphiwe@gmail.com>
2024-05-02 18:09:03 +0200

    variable change

--- WeatherForecast.WebAPI/WeatherForecast.cs
+++ WeatherForecast.WebAPI/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace WeatherForecast.WebAPI
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 30 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
